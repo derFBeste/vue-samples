317c2e9cca12f12c10271b6d0c29a3abdc1fdcd0
Fred Besteman <fbesteman@gmail.com>
2018-07-31 23:12:14 -0400

    Added DemoPopover.

---
+++ src/components/DemoPopover.vue
@@ -0,0 +1,42 @@
+<template>
+  <div
+    class="popover"
+    :style="position"
+    v-if="show"
+    @click="$emit('close')">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    show: { type: Boolean, required: true, default: false },
+    xCoordinate: { type: Number, required: true },
+    yCoordinate: { type: Number, required: true }
+  },
+  computed: {
+    position () {
+      return {
+        left: `${this.xCoordinate}px`,
+        top: `${this.yCoordinate}px`
+      }
+    }
+  },
+  methods: {
+    close: function () {
+      this.$emit('close')
+    }
+  }
+}
+</script>
+<style scoped>
+  .popover {
+    background-color: hsla(0,0%,100%,0.95);
+    box-shadow: 0 2px 8px rgba(0, 0, 0, .33);
+    padding: .5rem;
+    position: absolute;
+    transition: all .3s ease;
+    z-index: 999;
+  }
+</style>
--- src/components/Samples.vue
+++ src/components/Samples.vue
@@ -1,21 +1,52 @@
 <template>
   <section class="flex flex-column">
     <h1 class="ml3 tc">Sample Vue Components</h1>
-    <div class="bb b--black-30"></div>
-      <h3 class="ml3 tl">simple list with up down arrow control</h3>
-      <DemoList class="ml3" :input="japaneseFood"/>
+      <div class="bb b--black-30"></div>
+    <div class="flex flex-row">
+
+      <!-- Demo List -->
+      <div class="w-50">
+        <h3 class="ml3 tl">simple list with up down arrow control</h3>
+        <DemoList class="ml3" :input="japaneseFood"/>
+      </div>
+
+      <!-- Demo Popover -->
+      <div>
+        <h3 class="ml3 tl">popover example</h3>
+        <h4 class="ml5 pointer" @click="displayPopover($event)">click to show popover</h4>
+        <DemoPopover
+          :xCoordinate="xCoordinate"
+          :yCoordinate="yCoordinate"
+          :show="showPopover"
+          @close="showPopover = false">
+          <h3>How's it goin?</h3>
+        </DemoPopover>
+      </div>
+    </div>
   </section>
 </template>
 
 <script>
 import DemoList from './DemoList'
+import DemoPopover from './DemoPopover'
 
 export default {
   name: 'Samples',
-  components: {DemoList},
+  components: { DemoList, DemoPopover },
   data: () => ({
-    japaneseFood: [ 'takoyaki', 'okonomiyaki', 'yakitori', 'yakiniku', 'taiyaki' ]
-  })
+    japaneseFood: [ 'takoyaki', 'okonomiyaki', 'yakitori', 'yakiniku', 'taiyaki' ],
+    showPopover: false,
+    xCoordinate: 0,
+    yCoordinate: 0
+  }),
+
+  methods: {
+    displayPopover (event) {
+      this.xCoordinate = event.pageX
+      this.yCoordinate = event.pageY
+      this.showPopover = true
+    }
+  }
 }
 </script>
 
